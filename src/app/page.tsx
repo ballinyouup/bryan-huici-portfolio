4526fc83956d6400ba5e1480a8cb148caa719dea
ballinyouup <bryanhuici@gmail.com>
2023-05-28 21:23:18 -0400

    changed height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AnimatedItems from './animated-items'
 
 export default function Page() {
     return (
-        <main className="relative z-0 flex h-[640px] w-full items-center justify-center">
+        <main className="relative z-0 flex h-screen w-full items-center justify-center">
             <AnimatedItems />
         </main>
     )
