f838b11bc84b1b6c621a98664f291f9eb3026ae5
ballinyouup <bryanhuici@gmail.com>
2023-06-22 09:35:56 -0400

    split sections into components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
+import Hero from "@/components/ui/hero";
+import Skills from "@/components/ui/skills";
+
 export default function Page() {
     return (
         <>
-            <div className="flex h-screen items-center justify-center">
-                <div className="flex flex-col items-start justify-start gap-4 p-6 md:justify-center md:p-0">
-                    <h3 className="-ml-1 grid w-full grid-flow-row text-8xl font-semibold md:grid-flow-col">
-                        <span>BRYAN</span>
-                        <span className="hidden text-5xl md:flex">&nbsp;</span>
-                        <span>HUICI</span>
-                    </h3>
-                    <p className="grid w-fit grid-flow-row text-3xl font-semibold md:grid-flow-col">
-                        <span>Front-End</span>
-                        <span className="hidden md:flex">/</span>
-                        <span>Full-Stack</span>
-                        <span className="hidden text-base md:flex">&nbsp;</span>
-                        <span>Software Engineer</span>
-                    </p>
-                </div>
-            </div>
-            <div className="h-screen w-full bg-blue-300">test</div>
+            <Hero />
+            <Skills />
             <div className="h-screen w-full bg-gray-900">test</div>
         </>
     )
