0f9813ca1bccf7165f4c429f5465f2c596f25f51
ballinyouup <bryanhuici@gmail.com>
2023-06-22 11:29:21 -0400

    fixed arrow position

--- src/components/ui/down-arrow.tsx
+++ src/components/ui/down-arrow.tsx
@@ -5,6 +5,9 @@ export default function DownArrow() {
     return (
         <div>
             <motion.div
+                initial={{
+                    x: '-20px',
+                }}
                 animate={{
                     y: [0, -10, 0],
                 }}
@@ -13,7 +16,7 @@ export default function DownArrow() {
                     repeat: Infinity,
                     ease: 'linear',
                 }}
-                className="absolute bottom-20 left-[49%] z-10"
+                className="absolute bottom-20 left-1/2 z-10"
             >
                 <ChevronDown
                     className="h-10 w-10 rounded-full"
