a61b46a9bb0f4d0a30f313b24ad0b08b36c7e0a5
ballinyouup <bryanhuici@gmail.com>
2023-06-23 01:42:17 -0400

    added pulse to blur

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -3,7 +3,7 @@ import DownArrow from './down-arrow'
 export default function Hero() {
     return (
         <div className="relative flex h-phone flex-col items-center justify-center overflow-x-clip md:h-desktop">
-            <div className="z-20 flex h-full flex-col items-start justify-center gap-4 p-4 sm:p-6 md:p-4">
+            <div className="z-30 flex h-full flex-col items-start justify-center gap-4 p-4 sm:p-6 md:p-4">
                 <h3 className="-ml-1 grid w-full grid-flow-row text-8xl font-semibold md:grid-flow-col">
                     <span>BRYAN</span>
                     <span className="hidden text-5xl md:flex">&nbsp;</span>
@@ -17,8 +17,8 @@ export default function Hero() {
                     <span>Software Engineer</span>
                 </p>
             </div>
-            <div className="absolute left-1/4 top-1/2 z-20 h-full w-full -translate-y-1/2 translate-x-1/4 gap-4 opacity-30">
-                <div className="h-full w-full rounded-full bg-accent !blur-[64px] !brightness-200" />
+            <div className="absolute left-1/4 top-1/2 z-20 h-full w-full -translate-y-1/2 translate-x-1/4 gap-4 opacity-90">
+                <div className="h-full w-full animate-pulse rounded-full bg-accent !blur-[96px] !brightness-200" />
             </div>
             <DownArrow />
         </div>
