dc55ab5c68de62217200ae5f0d0fbfddca655873
ballinyouup <bryanhuici@gmail.com>
2023-06-23 01:42:07 -0400

    custom pulse animation settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -68,6 +68,7 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "pulse": "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite"
       },
       height: {
         'desktop': '60rem',
