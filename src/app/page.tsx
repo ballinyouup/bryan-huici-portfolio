fcaa81419ec501669f9dbf5c03d989c45bdbd702
ballinyouup <bryanhuici@gmail.com>
2023-06-23 04:22:06 -0400

    renamed skills to tech + z-index glow fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Hero from '@/components/ui/hero'
-import Skills from '@/components/ui/skills'
+import Technologies from '@/components/ui/technologies'
 
 export default function Page() {
     return (
         <main>
             <Hero />
-            <Skills />
+            <Technologies />
             <div className="h-desktop w-full bg-gray-900">test</div>
         </main>
     )
