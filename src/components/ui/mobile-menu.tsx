67bbee544ae4fd442a78bad1e9465d451938e680
ballinyouup <bryanhuici@gmail.com>
2023-06-23 23:30:29 -0400

    made mobile menu text uppercase + bold

--- src/components/ui/mobile-menu.tsx
+++ src/components/ui/mobile-menu.tsx
@@ -15,11 +15,11 @@ const MobileMenu = () => {
             <SheetTrigger>
                 <Menu className="h-8 w-8" />
             </SheetTrigger>
-            <SheetContent className="flex w-full flex-col text-2xl">
+            <SheetContent className="flex w-full flex-col text-2xl font-bold uppercase">
                 <SheetTitle className="text-3xl">Bryan Huici</SheetTitle>
                 <Link
                     href="/"
-                    className="flex items-center gap-1 p-1"
+                    className="flex items-center gap-1"
                     onClick={handleOpen}
                 >
                     <Home className="h-6 w-6" />
@@ -27,7 +27,7 @@ const MobileMenu = () => {
                 </Link>
                 <Link
                     href="/projects"
-                    className="flex items-center gap-1 p-1"
+                    className="flex items-center gap-1"
                     onClick={handleOpen}
                 >
                     <FolderGit2 className="h-6 w-6" />
@@ -35,7 +35,7 @@ const MobileMenu = () => {
                 </Link>
                 <Link
                     href="/resume"
-                    className="flex items-center gap-1 p-1"
+                    className="flex items-center gap-1"
                     onClick={handleOpen}
                 >
                     <ScrollText className="h-6 w-6" />
