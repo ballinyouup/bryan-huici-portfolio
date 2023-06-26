b0d2ae701d9d4e1f64ae0bf61491c728e001a4b7
ballinyouup <bryanhuici@gmail.com>
2023-06-26 08:02:17 -0400

    fixed typescript error

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,6 +2,6 @@ export const apiVersion = '2023-06-26'
 
 export const dataset = 'production'
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string
 
 export const useCdn = false
