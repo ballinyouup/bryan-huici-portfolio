a2404b449d8cd833838dd3820312b980a904d7ab
ballinyouup <bryanhuici@gmail.com>
2023-06-26 08:13:32 -0400

    added projects to sanity

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,10 +1,11 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
-import blockContent from './schemas/blockContent.ts'
-import category from './schemas/category.ts'
-import post from './schemas/post.ts'
-import author from './schemas/author.ts'
+import blockContent from './schemas/blockContent'
+import category from './schemas/category'
+import post from './schemas/post'
+import author from './schemas/author'
+import projects from './schemas/projects'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent],
+    types: [post, author, category, blockContent, projects],
 }
