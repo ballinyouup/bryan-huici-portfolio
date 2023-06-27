0f9c85382b4fe217e384b3a8171211be0565cb89
ballinyouup <bryanhuici@gmail.com>
2023-06-27 15:00:38 -0400

    testing og fix

--- src/app/(portfolio)/blog/[slug]/opengraph-image.tsx
+++ src/app/(portfolio)/blog/[slug]/opengraph-image.tsx
@@ -1,6 +1,5 @@
 import { ImageResponse } from 'next/server'
-import { Inter } from 'next/font/google'
-
+import { client } from '../../../../../sanity/lib/client.mts'
 // Route segment config
 // export const runtime = 'edge'
 
@@ -13,40 +12,37 @@ export const size = {
 
 export const contentType = 'image/png'
 
-// Font
-const inter = Inter({ subsets: ['latin'] })
-
 // Image generation
 export default async function Image({ params }: { params: { slug: string } }) {
-    const post = await fetch(`/posts/${params.slug}`).then((res) =>
-    res.json()
-  ) as {title: string}
-  return new ImageResponse(
-      (
-          // ImageResponse JSX element
-          <div
-              style={{
-                  fontSize: 128,
-                  background: 'black',
-                  width: '100%',
-                  height: '100%',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  color: 'white',
-                  fontWeight: 'bold',
-                  textTransform: 'capitalize',
-              }}
-              className={inter.className}
-          >
-              {JSON.stringify(post.title)}
-          </div>
-      ),
-      // ImageResponse options
-      {
-          // For convenience, we can re-use the exported opengraph-image
-          // size config to also set the ImageResponse's width and height.
-          ...size,
-      }
-  )
+    const post = await client.fetch(
+        `*[slug.current == "${params.slug}"]{title}`
+    )
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    fontSize: 128,
+                    background: 'black',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'white',
+                    fontWeight: 'bold',
+                    textTransform: 'capitalize',
+                }}
+            >
+                {post.title}
+            </div>
+        ),
+        // ImageResponse options
+        {
+            // For convenience, we can re-use the exported opengraph-image
+            // size config to also set the ImageResponse's width and height.
+            ...size,
+            debug: true,
+        }
+    )
 }
