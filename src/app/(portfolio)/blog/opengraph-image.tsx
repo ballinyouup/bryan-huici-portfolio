37477e6bbb7362d6ab7a637326f3d175cf9c7292
ballinyouup <bryanhuici@gmail.com>
2023-06-27 18:49:47 -0400

    added og to /blog

---
+++ src/app/(portfolio)/blog/opengraph-image.tsx
@@ -0,0 +1,43 @@
+import { ImageResponse } from 'next/server'
+// Route segment config
+// export const runtime = 'edge'
+
+// Image metadata
+export const alt = 'Bryan Huici Blog Post Image'
+export const size = {
+    width: 1200,
+    height: 630,
+}
+
+export const contentType = 'image/png'
+
+// Image generation
+export default async function Image({ params }: { params: { slug: string } }) {
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    fontSize: 128,
+                    background: 'black',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'white',
+                    fontWeight: 'bold',
+                    textTransform: 'uppercase',
+                }}
+            >
+                Blog
+            </div>
+        ),
+        // ImageResponse options
+        {
+            // For convenience, we can re-use the exported opengraph-image
+            // size config to also set the ImageResponse's width and height.
+            ...size,
+        }
+    )
+}
