c01756fb657f4ef9c536a1db2a9277398ec4fd67
ballinyouup <bryanhuici@gmail.com>
2023-06-29 18:07:30 -0400

    added prettier formatting

--- src/app/(portfolio)/blog/page.tsx
+++ src/app/(portfolio)/blog/page.tsx
@@ -15,12 +15,14 @@ interface Post {
     title: string;
     publishedAt: string;
     image: Image2;
+    keywords: string[];
 }
 
 export default async function Page() {
-    const posts = await client.fetch(`*[_type == "post"]| order(publishedAt desc){
+    const posts =
+        await client.fetch(`*[_type == "post"]| order(publishedAt desc){
 		"author": author->name,
-		  description, title, slug, publishedAt, "image": mainImage
+		  description, title, slug, publishedAt, "image": mainImage, keywords
 	  }`);
 
     return (
@@ -30,16 +32,16 @@ export default async function Page() {
                     <Link
                         href={`/blog/${post.slug.current}`}
                         key={post.slug.current}
-                        className="flex md:flex-row flex-col h-fit md:h-80 w-full max-w-5xl hover:bg-accent transition-all p-2 overflow-hidden gap-2"
+                        className="flex h-fit w-full max-w-5xl flex-col gap-2 overflow-hidden p-2 transition-all hover:bg-accent md:h-80 md:flex-row"
                         passHref
                     >
-                        <div className='h-full w-full overflow-hidden'>
+                        <div className="h-full w-full overflow-hidden">
                             {post.image ? (
                                 <Image
                                     src={urlForImage(post.image)
                                         .width(1600)
                                         .height(1600)
-                                        .fit("max")
+                                        .fit('max')
                                         .auto('format')
                                         .url()}
                                     alt={post.image.alt as string}
@@ -48,20 +50,35 @@ export default async function Page() {
                                 />
                             ) : null}
                         </div>
-                        <div className="flex flex-col h-fit md:h-full w-full gap-3">
+                        <div className="flex h-fit w-full flex-col gap-3 md:h-full">
                             <div>
                                 <h5 className="font-bold">{post.title}</h5>
-                                <div className='flex gap-2'>
-                                    <p className="text-base font-medium">By: {post.author}</p>
-                                    <Badge>
+                                <div className="flex gap-2">
+                                    <p className="text-base font-medium">
+                                        By: {post.author}
+                                    </p>
+                                    <p>
                                         {format(
                                             new Date(post.publishedAt),
                                             'MM-dd-yyyy',
                                             { locale: enUS }
                                         )}
-                                    </Badge>
+                                    </p>
                                 </div>
                             </div>
+                            {post.keywords ? (
+                                <div className="flex gap-2">
+                                    {post.keywords
+                                        .slice(0, 4)
+                                        .map((keyword) => {
+                                            return (
+                                                <Badge key={keyword}>
+                                                    {keyword}
+                                                </Badge>
+                                            );
+                                        })}
+                                </div>
+                            ) : null}
                             <p className="line-clamp-3">{post.description}</p>
                         </div>
                     </Link>
