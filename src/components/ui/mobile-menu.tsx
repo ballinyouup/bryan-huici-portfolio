0a5a682521302e9b16135ea17810d30b02eee7a9
ballinyouup <bryanhuici@gmail.com>
2023-06-29 22:48:13 -0400

    adjusted mobile menu text size and gap

--- src/components/ui/mobile-menu.tsx
+++ src/components/ui/mobile-menu.tsx
@@ -15,11 +15,11 @@ const MobileMenu = () => {
             <SheetTrigger>
                 <Menu className="h-8 w-8" />
             </SheetTrigger>
-            <SheetContent className="flex w-full flex-col text-2xl font-bold uppercase">
-                <SheetTitle className="text-3xl">Bryan Huici</SheetTitle>
+            <SheetContent className="flex w-full flex-col text-3xl font-bold uppercase">
+                <SheetTitle className="text-[10vw]">Bryan Huici</SheetTitle>
                 <Link
                     href="/"
-                    className="flex items-center gap-1"
+                    className="flex items-center gap-4"
                     onClick={handleOpen}
                 >
                     <Home className="h-6 w-6" />
@@ -27,7 +27,7 @@ const MobileMenu = () => {
                 </Link>
                 <Link
                     href="/blog"
-                    className="flex items-center gap-1"
+                    className="flex items-center gap-4"
                     onClick={handleOpen}
                 >
                     <FileType2 className="h-6 w-6" />
@@ -35,7 +35,7 @@ const MobileMenu = () => {
                 </Link>
                 <Link
                     href="/projects"
-                    className="flex items-center gap-1"
+                    className="flex items-center gap-4"
                     onClick={handleOpen}
                 >
                     <FolderGit2 className="h-6 w-6" />
@@ -43,7 +43,7 @@ const MobileMenu = () => {
                 </Link>
                 <Link
                     href="/resume"
-                    className="flex items-center gap-1"
+                    className="flex items-center gap-4"
                     onClick={handleOpen}
                 >
                     <ScrollText className="h-6 w-6" />
