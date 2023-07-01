c952c0fa47f9dc4dcaab5bb5459427f7b79c0860
ballinyouup <bryanhuici@gmail.com>
2023-06-30 20:33:23 -0400

    formatting

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -2,11 +2,9 @@ import DownArrow from './down-arrow'
 import { GradualSpacing } from '../animations/hero-text'
 export default function Hero() {
     return (
-        <div className="relative flex h-phone flex-col items-center justify-center overflow-x-clip md:h-desktop snap-start">
+        <div className="relative flex h-phone snap-start flex-col items-center justify-center overflow-x-clip md:h-desktop">
             <div className="z-20 flex h-fit flex-col items-start justify-center gap-4 p-8 sm:p-6 md:p-4">
-
-                    <GradualSpacing />
-
+                <GradualSpacing />
                 <p className="grid w-fit grid-flow-row text-3xl font-semibold md:grid-flow-col">
                     <span>Front-End</span>
                     <span className="hidden md:flex">/</span>
