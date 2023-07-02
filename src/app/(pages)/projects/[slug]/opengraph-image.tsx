93c50b81582f0fbf26e0c5b2e95e926bf2b40312
ballinyouup <bryanhuici@gmail.com>
2023-07-01 22:33:11 -0400

    styled open graph images

--- src/app/(pages)/projects/[slug]/opengraph-image.tsx
+++ src/app/(pages)/projects/[slug]/opengraph-image.tsx
@@ -15,26 +15,81 @@ export const contentType = 'image/png'
 // Image generation
 export default async function Image({ params }: { params: { slug: string } }) {
     const project = await client.fetch(
-        `*[slug.current == "${params.slug}"]{title}`
+        `*[slug.current == "${params.slug}"]{title, description}`
     )
     return new ImageResponse(
         (
             // ImageResponse JSX element
-            <div
-                style={{
-                    fontSize: 128,
-                    background: 'black',
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: 'white',
-                    fontWeight: 'bolder',
-                    textTransform: 'uppercase',
+            <div style={{
+                width: '100%',
+                height: '100%',
+                display: "flex",
+                backgroundImage: 'linear-gradient(-45deg, #1D283A, #000000, #000000)',
+                position: "relative",
+                zIndex: 1
+            }}>
+                <span
+                    style={{
+                        fontSize: 36,
+                        textTransform: 'uppercase',
+                        fontWeight: 900,
+                        color: 'white',
+                        position: 'absolute',
+                        bottom: 4,
+                        zIndex: 1,
+                        width: '100%',
+                        height: '145px',
+                        padding: '40px 0px 20px 46px',
+                        backgroundImage: 'linear-gradient(-45deg, #0000000, transparent, transparent)',
+                        overflow: 'hidden',
+                        whiteSpace: 'normal',
+                        textOverflow: 'ellipsis'
+                    }}
+                >
+                    {project[0].description}
+                </span>
+
+                <span
+                    style={{
+                        fontSize: 96,
+                        textTransform: 'uppercase',
+                        fontWeight: 900,
+                        color: 'white',
+                        position: "absolute",
+                        top: 120,
+                        zIndex: 1,
+                        width: "50%",
+                        height: "370px",
+                        padding: "20px 0px 0px 42px",
+                        overflow: 'hidden',
+                        overflowWrap: "break-word",
+                        lineHeight: "90px"
                 }}
-            >
+                >
                 {project[0].title}
+                </span>
+
+                <span
+                    style={{
+                        fontSize: 52,
+                        textTransform: 'uppercase',
+                        fontWeight: 900,
+                        color: 'white',
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        zIndex: 1,
+                        padding: "20px 40px",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "flex-start",
+                        gap: "12px",
+                        backgroundImage: 'linear-gradient(45deg, #1D283A, transparent)',
+                        width: "100%"
+                    }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-dribbble"><circle cx="12" cy="12" r="10" /><path d="M19.13 5.09C15.22 9.14 10 10.44 2.25 10.94" /><path d="M21.75 12.84c-6.62-1.41-12.14 1-16.38 6.32" /><path d="M8.56 2.75c4.37 6 6 9.42 8 17.72" /></svg>
+                    BRYAN HUICI
+                </span>
             </div>
         ),
         // ImageResponse options
