40f9704091bd6b8f2be68ca11071537e254dfd8b
ballinyouup <bryanhuici@gmail.com>
2023-07-04 01:23:40 -0400

    fixed positioning for down arrow

--- src/components/ui/down-arrow.tsx
+++ src/components/ui/down-arrow.tsx
@@ -4,7 +4,7 @@ export default function DownArrow({ section }: { section: number }) {
     return (
         <>
             <button
-                className="absolute bottom-20 left-1/2 z-40 hidden !-translate-x-6 animate-bounce rounded-xl border-2 border-transparent bg-accent transition-transform duration-1000 hover:border-white md:flex"
+                className="absolute bottom-20 left-1/2 z-40 hidden !-translate-x-1/2 animate-bounce rounded-xl border-2 border-transparent bg-accent transition-transform duration-1000 hover:border-white md:flex"
                 onClick={() =>
                     scrollTo({ top: 960 * section, behavior: 'smooth' })
                 }
@@ -15,7 +15,7 @@ export default function DownArrow({ section }: { section: number }) {
                 <ChevronDown className="h-10 w-10" strokeWidth={'4px'} />
             </button>
             <button
-                className="absolute bottom-20 left-1/2 z-40 flex !-translate-x-4 animate-bounce rounded-xl border-2 border-transparent bg-accent transition-transform duration-1000 hover:border-white md:hidden"
+                className="absolute bottom-20 left-1/2 z-40 flex !-translate-x-1/2 animate-bounce rounded-xl border-2 border-transparent bg-accent transition-transform duration-1000 hover:border-white md:hidden"
                 onClick={() =>
                     scrollTo({ top: 704 * section, behavior: 'smooth' })
                 }
