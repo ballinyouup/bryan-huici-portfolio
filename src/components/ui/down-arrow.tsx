18417d64a3468fb20773681c73026ae011e2cdaf
ballinyouup <bryanhuici@gmail.com>
2023-07-03 23:20:55 -0400

    fixed arrow for mobile

--- src/components/ui/down-arrow.tsx
+++ src/components/ui/down-arrow.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { ChevronDown } from 'lucide-react'
 import { motion } from 'framer-motion'
-export default function DownArrow({ section }: { section: number; }) {
+export default function DownArrow({ section }: { section: number }) {
     return (
-
-        <motion.button
-            initial={{
-                x: -36
-            }}
+        <>
+            <motion.button
+                initial={{
+                    x: -36,
+                }}
                 animate={{
                     y: [0, -10, 0],
                 }}
@@ -16,14 +16,32 @@ export default function DownArrow({ section }: { section: number; }) {
                     repeat: Infinity,
                     ease: 'linear',
                 }}
-            className="absolute bottom-20 left-1/2 z-40 border-2 border-transparent hover:border-white rounded-xl transition-all duration-300"
-            onClick={() => scrollTo({ top: 960 * section, behavior: "smooth" })}
+                className="absolute bottom-20 left-1/2 z-40 hidden rounded-xl border-2 border-transparent transition-all duration-300 hover:border-white md:flex"
+                onClick={() =>
+                    scrollTo({ top: 960 * section, behavior: 'smooth' })
+                }
             >
-                <ChevronDown
-                className="h-10 w-10"
-                    strokeWidth={'4px'}
-                />
-        </motion.button>
-
+                <ChevronDown className="h-10 w-10" strokeWidth={'4px'} />
+            </motion.button>
+            <motion.button
+                initial={{
+                    x: -36,
+                }}
+                animate={{
+                    y: [0, -10, 0],
+                }}
+                transition={{
+                    duration: 3,
+                    repeat: Infinity,
+                    ease: 'linear',
+                }}
+                className="absolute bottom-20 left-1/2 z-40 hidden rounded-xl border-2 border-transparent transition-all duration-300 hover:border-white md:flex"
+                onClick={() =>
+                    scrollTo({ top: 704 * section, behavior: 'smooth' })
+                }
+            >
+                <ChevronDown className="h-10 w-10" strokeWidth={'4px'} />
+            </motion.button>
+        </>
     )
 }
