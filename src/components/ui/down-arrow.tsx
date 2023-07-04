5ba6dbbf1423c934e871e24c0d0adc56a9ea49f5
ballinyouup <bryanhuici@gmail.com>
2023-07-03 23:24:00 -0400

    added bounce to arrow

--- src/components/ui/down-arrow.tsx
+++ src/components/ui/down-arrow.tsx
@@ -1,47 +1,25 @@
-'use client'
 import { ChevronDown } from 'lucide-react'
-import { motion } from 'framer-motion'
 export default function DownArrow({ section }: { section: number }) {
     return (
         <>
-            <motion.button
-                initial={{
-                    x: -36,
-                }}
-                animate={{
-                    y: [0, -10, 0],
-                }}
-                transition={{
-                    duration: 3,
-                    repeat: Infinity,
-                    ease: 'linear',
-                }}
-                className="absolute bottom-20 left-1/2 z-40 hidden rounded-xl border-2 border-transparent transition-all duration-300 hover:border-white md:flex"
+            <button
+
+                className="absolute bottom-20 animate-bounce duration-1000 left-1/2 z-40 hidden rounded-xl border-2 border-transparent transition-transform hover:border-white md:flex"
                 onClick={() =>
                     scrollTo({ top: 960 * section, behavior: 'smooth' })
                 }
             >
                 <ChevronDown className="h-10 w-10" strokeWidth={'4px'} />
-            </motion.button>
-            <motion.button
-                initial={{
-                    x: -36,
-                }}
-                animate={{
-                    y: [0, -10, 0],
-                }}
-                transition={{
-                    duration: 3,
-                    repeat: Infinity,
-                    ease: 'linear',
-                }}
-                className="absolute bottom-20 left-1/2 z-40 hidden rounded-xl border-2 border-transparent transition-all duration-300 hover:border-white md:flex"
+            </button>
+            <button
+
+                className="absolute animate-bounce bottom-20 left-1/2 z-40 flex rounded-xl border-2 border-transparent transition-transform duration-1000 hover:border-white md:hidden"
                 onClick={() =>
                     scrollTo({ top: 704 * section, behavior: 'smooth' })
                 }
             >
                 <ChevronDown className="h-10 w-10" strokeWidth={'4px'} />
-            </motion.button>
+            </button>
         </>
     )
 }
