0a3060bf3ff06e1f2390285a8091505f1c5bc393
ballinyouup <bryanhuici@gmail.com>
2025-12-06 19:07:15 -0500

    fixed import

--- src/app/(pages)/blog/[slug]/opengraph-image.tsx
+++ src/app/(pages)/blog/[slug]/opengraph-image.tsx
@@ -1,4 +1,4 @@
-import { ImageResponse } from 'next/server'
+import { ImageResponse } from 'next/og'
 import { client } from '~/lib/client.mts'
 // Route segment config
 // export const runtime = 'edge'
